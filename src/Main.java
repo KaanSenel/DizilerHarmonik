0d82d099b614666d8e3a468e022580b276006cd8
Kaan Şenel <tufankaansenel@gmail.com>
2022-02-07 17:06:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] numbers={12,4,6,32,21,9,3};
+        double sum=0.0;
+        for (int i=0;i<numbers.length;i++){
+            sum+=1/(1.0+numbers[i]);
+        }
+
+        double average=numbers.length/sum;
+        System.out.println(average);
+    }
+}
